e05b3e55f42cf69f5fe0c2f4440f882c00acdd8d
Shaikh Irshad Ahmed <162448547+Irshad45678@users.noreply.github.com>
2025-08-15 22:18:55 +0530

    Add files via upload

---
+++ PythonL7(IF).py
@@ -0,0 +1,11 @@
+#10. If Statements
+x = 10
+if x > 0:
+    print("Positive")
+elif x == 0:
+    print("Zero")
+else:
+    print("Negative")
+
+# One-liner
+print("Positive") if x > 0 else print("Non-positive")
